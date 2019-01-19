d5f725f41bb4447d76b599d5812a1db7f9722211
moh3 <anisi.1976@gmail.com>
2019-01-19 23:22:23 +0330

    Deploy to Heroku

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -1,12 +1,14 @@
 import { Route } from '@angular/router';
-
+import { UserRouteAccessService } from 'app/core/';
 import { HomeComponent } from './';
-
+import { HI_THERE_ROUTE } from 'app/hi-there/hi-there.route';
 export const HOME_ROUTE: Route = {
     path: '',
     component: HomeComponent,
     data: {
         authorities: [],
         pageTitle: 'home.title'
-    }
+    },
+    canActivate: [UserRouteAccessService],
+    children: [HI_THERE_ROUTE]
 };
